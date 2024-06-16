d05d37271632f5d38b40580cff10621b969da781
naayuzahem <165249722+naayuzahem@users.noreply.github.com>
2024-06-16 21:42:16 +0800

    change databasename

--- parkingpage.php
+++ parkingpage.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "manageparkingarea";
+$dbname = "fkpark";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
